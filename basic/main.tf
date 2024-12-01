ad1a24d9c2587a619a6c3fdc84a1fed42581bad3
Neeraj Gupta <gneeraj970@gmail.com>
2024-12-01 19:47:39 +0530

    Added main.tf file

---
+++ basic/main.tf
@@ -0,0 +1,40 @@
+# Using the following types of input variables.
+# String, Number, Bool, List, Map
+resource "local_file" "car-1" {
+  filename = "/opt/car-1.txt"
+  content  = <<EOT
+  Car details...
+  Name: ${var.car_make}
+  Year: ${var.car_year}
+  Is hybrid: ${var.is_hybrid}
+  Feature: ${var.car_features[1]}
+  Color: ${var.car_tags["color"]}
+  EOT
+}
+
+# Using the following types of input variables.
+# Object
+resource "local_file" "car-2" {
+  filename = "/opt/car-2.txt"
+  content  = <<EOT
+  Car details...
+  Name: ${var.car_config.make}
+  Year: ${var.car_config.year}
+  Is hybrid: ${var.car_config.hybrid}
+  Feature: ${var.car_config.feature[0]}
+  EOT
+}
+
+# Using the following types of input variables.
+# Tuple
+resource "local_file" "car-3" {
+  filename = "/opt/car-3.txt"
+  content  = <<EOT
+  Car details...
+  Name: ${var.car_data[0]}
+  Year: ${var.car_data[1]}
+  Is hybrid: ${var.car_data[2]}
+  Feature: ${var.car_data[3][2]}
+  EOT
+}
+
